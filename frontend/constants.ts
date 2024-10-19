59699b0b02ef47405eb336da15147f579db12801
kunaldhongade <kunaldhongade1@gmail.com>
2024-10-19 13:03:42 +0530

    Update: Project address and readme

--- frontend/constants.ts
+++ frontend/constants.ts
@@ -1,5 +1,5 @@
 export const NETWORK = import.meta.env.VITE_APP_NETWORK ?? "testnet";
-export const MODULE_ADDRESS = "0x7792db2bc2e3c11f4485060e1112fab7a9d88d971af5cc638a9a486d6fb7ca61";
+export const MODULE_ADDRESS = import.meta.env.VITE_MODULE_ADDRESS;
 export const CREATOR_ADDRESS = import.meta.env.VITE_COLLECTION_CREATOR_ADDRESS;
 export const COLLECTION_ADDRESS = import.meta.env.VITE_COLLECTION_ADDRESS;
 export const IS_DEV = Boolean(import.meta.env.DEV);
